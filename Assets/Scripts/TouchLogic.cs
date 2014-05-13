82ca8278c569c924547a7dacfafc37ed72d4469f
mcelikbas <mikail.celikbas@gmail.com>
2014-05-13 14:54:25 -0400

    amelioration deplacement cube

--- Assets/Scripts/TouchLogic.cs
+++ Assets/Scripts/TouchLogic.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TouchLogic : MonoBehaviour {
 
-	void Update (){
+	public void CheckTouches (){
 
 		// is there a touch on screen?
 		if (Input.touches.Length <= 0) {
@@ -15,28 +15,32 @@ public class TouchLogic : MonoBehaviour {
 
 			// loop through all touches
 			for (int i = 0; i < Input.touchCount; i++) {
-				if(this.guiTexture.HitTest(Input.GetTouch(i).position)) {
-					// the current touch is hitting our guiTexture
+				if(this.guiTexture != null && this.guiTexture.HitTest(Input.GetTouch(i).position)) {
+				// the current touch is hitting our guiTexture
+
 					if(Input.GetTouch(i).phase == TouchPhase.Began){
-						// A finger touched the screen
+					// A finger touched the screen
 
 						// calls the function "OnTouchBegan"
 						this.SendMessage("OnTouchBegan");
-						//Debug.Log("Touch began on:" + this.name);
 					}
 					if(Input.GetTouch(i).phase == TouchPhase.Ended){
-						// A finger was lifted from the screen
+					// A finger was lifted from the screen
 
 						// calls the function "OnTouchEnd"
 						this.SendMessage("OnTouchEnd");
-						//Debug.Log("Touch ended on:" + this.name);
 					}
 					if(Input.GetTouch(i).phase == TouchPhase.Stationary){
-						// A finger is touching the screen but hasn't moved
+					// A finger is touching the screen but hasn't moved
 						
 						// calls the function "OnTouchStationary"
 						this.SendMessage("OnTouchStationary");
-						//Debug.Log("Touch stationary on:" + this.name);
+					}
+					if(Input.GetTouch(i).phase == TouchPhase.Moved){
+						// A finger moved on the screen.
+						
+						// calls the function "OnTouchMoved"
+						this.SendMessage("OnTouchMoved");
 					}
 				}
 
--- Assets/Scripts/TouchTest.cs
+++ Assets/Scripts/TouchTest.cs
@@ -5,27 +5,49 @@ public class TouchTest : TouchLogic {
 
 	public Transform myCube;
 	public float dir = 0.0f;
+	public bool canTouch = true;
+
+	void Update() {
+		if (canTouch) {
+		// we can touch the screen
+			CheckTouches();
+		}
+	}
 
 	void OnTouchBegan () {
-		//Debug.Log("Touch began on:" + this.name);
 		if (this.name == "Right Button") {
+		// move right
 			dir = 1.0f;
 		}
 		else if (this.name == "Left Button") {
+		// move left
 			dir = -1.0f;
 		}
+		else if (this.name == "Enable Touch") {
+		// we enable/disable touches for all GUItexture except the one that allow us to switch between states
+			GUITexture[] guiTex = FindObjectsOfType(typeof(GUITexture)) as GUITexture[];
+			foreach (GUITexture tex in guiTex) {
+				if (tex.name != "Enable Touch") {
+					tex.GetComponent<TouchTest>().canTouch = !(tex.GetComponent<TouchTest>().canTouch);
+				}
+			}
+			
+		}
+
 		Camera.main.backgroundColor = Color.red;
 		myCube.Translate(10*Time.deltaTime*dir,0,0);
 	}
 
 	void OnTouchEnd () {
-		//Debug.Log("Touch ended on:" + this.name);
 		Camera.main.backgroundColor = Color.blue;
 		dir = 0.0f;
 	}
 
 	void OnTouchStationary () {
-		//Debug.Log("Touch stationary on:" + this.name);
 		myCube.Translate(10*Time.deltaTime*dir,0,0);
 	}
+
+	void OnTouchMoved () {
+
+	} 
 }
