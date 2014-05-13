d722d6d12bba1e945d7f961dc91bbd27c5ee0594
mcelikbas <mikail.celikbas@gmail.com>
2014-05-13 11:38:40 -0400

    ajout feature1-testA

---
+++ Assets/Scripts/testA.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testA : MonoBehaviour {
+
+
+
+	void Update () {
+
+		transform.Translate(20*Time.deltaTime*Input.GetAxisRaw("Horizontal"),0,0);
+
+	}
+}
