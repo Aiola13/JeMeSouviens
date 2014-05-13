fc3fe310a09a208ab0550652c8fa19fe4cc6b744
SullivanKowal <Martibide@gmail.com>
2014-05-13 12:38:03 -0400

    Upload voiture.unity

--- Assets/Scripts/testA.cs
+++ Assets/Scripts/testA.cs
@@ -1,13 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class testA : MonoBehaviour {
-
-
-
+public class testA: MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
 	void Update () {
-
-		transform.Translate(20*Time.deltaTime*Input.GetAxisRaw("Horizontal"),0,0);
-
+		
+		Vector3 move = new Vector3();
+		
+		if (Input.GetKey(KeyCode.LeftArrow))
+			move.x -= 0.1f;
+		if (Input.GetKey(KeyCode.RightArrow))
+			move.x += 0.1f;
+		
+		transform.position += move;
+		
 	}
 }
