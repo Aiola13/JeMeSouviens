0f69e0533a54c0d3ea6488a0658e791791e6e94c
MaGOs92 <guillaume_fay@hotmail.com>
2014-05-15 16:52:42 -0400

    script de quete OK

--- Assets/Scripts/QueteCrepe.cs
+++ Assets/Scripts/QueteCrepe.cs
@@ -7,7 +7,7 @@ public class QueteCrepe : MonoBehaviour {
     public GameObject ing_farine;
     public GameObject ing_lait;
     public GameObject ing_oeuf;
-   // public GameObject ing_sel;
+    public GameObject ing_sel;
     public GameObject ing_vanille;
     public GameObject ing_sirop_erable;
     public GameObject ing_pomme;
@@ -19,13 +19,24 @@ public class QueteCrepe : MonoBehaviour {
 
     GameObject[] liste_ing;
 
-    int[] ingDejaTire;
+    int nbOptionnels;
+    int[] ingDejaTireOpt;
+
+    int[] ingDejaTireObl;
 
     bool showGUI = true;
 
 	// Use this for initialization
 	void Start () {
-        creerListe();
+        
+        nbOptionnels = Random.Range(1, 4);
+        
+        liste_ing = new GameObject[3 + nbOptionnels];
+        
+        ingredientsObligatoires();
+        
+        ingredientsOptionnels();
+        
         afficherListeDebug();
 	}
 	
@@ -35,24 +46,9 @@ public class QueteCrepe : MonoBehaviour {
 
 	}
 
-    void creerListe() {
-
-        // On détermine combien on veut d'ingrédients optionnels (entre 1 et 3)
-
-        int nbOptionnels = Random.Range(1, 4);
-
-        // Ajout des ingrédients obligatoires dans le tableau
-
-        liste_ing = new GameObject[3 + nbOptionnels];
-
-        liste_ing[0] = ing_sucre;
-        liste_ing[1] = ing_farine;
-        liste_ing[2] = ing_lait;
-        liste_ing[3] = ing_oeuf;
-
-        // Ajout des ingrédients optionnels ds le tableau
-
-        ingDejaTire = new int[nbOptionnels];
+    void ingredientsOptionnels()
+    {
+        ingDejaTireOpt = new int[nbOptionnels];
 
         int j = 0;
 
@@ -62,12 +58,12 @@ public class QueteCrepe : MonoBehaviour {
 
             if (j == 0)
             {
-                ingDejaTire[j] = ingredientOpt;
+                ingDejaTireOpt[j] = ingredientOpt;
                 j++;
             }
             else
             {
-                while (verifDejaTire(ingredientOpt, j))
+                while (verifDejaTire(ingredientOpt, j, ingDejaTireOpt))
                 {
                     ingredientOpt = Random.Range(1, 7);
                 }
@@ -76,7 +72,7 @@ public class QueteCrepe : MonoBehaviour {
 
             switch (ingredientOpt)
             {
-                case 1 :
+                case 1:
                     liste_ing[i] = ing_vanille;
                     break;
 
@@ -102,16 +98,62 @@ public class QueteCrepe : MonoBehaviour {
 
             }
         }
-
     }
 
-    bool verifDejaTire(int numIng, int nbIngDejaTire)
+    void ingredientsObligatoires()
+    {
+        ingDejaTireObl = new int[3];
+
+
+        for (int i = 0; i < 3; i++)
+        {
+            int ingredientObl = Random.Range(1, 6);
+
+            if (i == 0)
+            {
+                ingDejaTireObl[i] = ingredientObl;
+            }
+            else
+            {
+                while (verifDejaTire(ingredientObl, i, ingDejaTireObl))
+                {
+                    ingredientObl = Random.Range(1, 6);
+                }
+            }
+
+            switch (ingredientObl)
+            {
+                case 1:
+                    liste_ing[i] = ing_sucre;
+                    break;
+
+                case 2:
+                    liste_ing[i] = ing_farine;
+                    break;
+
+                case 3:
+                    liste_ing[i] = ing_lait;
+                    break;
+
+                case 4:
+                    liste_ing[i] = ing_oeuf;
+                    break;
+
+                case 5:
+                    liste_ing[i] = ing_sel;
+                    break;
+
+            }
+        }
+    }
+
+    bool verifDejaTire(int numIng, int nbIngDejaTire, int[] tab)
     {
         bool verif = false;
 
         for (int i = 0; i < nbIngDejaTire; i++)
         {
-            if (numIng == ingDejaTire[i])
+            if (numIng == tab[i])
             {
                 verif = true;
             }
@@ -160,13 +202,60 @@ public class QueteCrepe : MonoBehaviour {
     string texteQuete()
     {
         string quete = "Bonjour que dirais-tu de m'aider à préparer des crêpes?\n";
-        quete += "Pour la crêpe d'aujourd'hui nous avons besoin des ingrédients suivants :\n";
+        quete += "J'ai déjà mis quelques ingrédients dans le saladier!\n";
+        quete += "Voici les ingrédients que tu dois ajouter pour finir la pâte :\n";
         for (int i = 0; i < liste_ing.Length; i++)
         {
-            quete += "- " + liste_ing[i].tag + "\n";
+            quete += "- " + nomIngredient(liste_ing[i].tag) + "\n";
         }
 
             return quete;
+    }
+
+    string nomIngredient(string tag)
+    {
+        string nomIng = "";
+
+        switch (tag)
+        {
+            case "ing_abricot":
+                nomIng = "un abricot";
+                break;
+            case "ing_sucre":
+                nomIng = "du sucre";
+                break;
+            case "ing_farine":
+                nomIng = "de la farine";
+                break;
+            case "ing_lait":
+                nomIng = "du lait";
+                break;
+            case "ing_oeuf":
+                nomIng = "des oeufs";
+                break;
+            case "ing_sel":
+                nomIng = "du sel";
+                break;
+            case "ing_vanille":
+                nomIng = "de la vanille";
+                break;
+            case "ing_sirop_erable":
+                nomIng = "du sirop d'érable";
+                break;
+            case "ing_pomme":
+                nomIng = "une pomme";
+                break;
+            case "ing_rhum":
+                nomIng = "du rhum";
+                break;
+            case "ing_fleur_oranger":
+                nomIng = "de la fleur d'oranger";
+                break;
+
+        }
+
+        return nomIng;
+
     }
 
 }
