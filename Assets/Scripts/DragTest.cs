0634c394ec2bb57019ccc1a09d658bd72a03c1f6
mcelikbas <mikail.celikbas@gmail.com>
2014-05-15 17:28:25 -0400

    modif drag

--- Assets/Scripts/DragTest.cs
+++ Assets/Scripts/DragTest.cs
@@ -3,36 +3,53 @@ using System.Collections;
 
 public class DragTest : MonoBehaviour, I3dObjectTouchable {
 
-	//Camera.main.backgroundColor = Color.red;
+	public float camDepth;
+	public bool isDragging = false;
+	public int lastTouch = 0;			// we allow the drag only with the first finger touched
 
 	public Vector3 curPos;
 	public Vector3 startPos;
 	public Vector3 endPos;
 
-	public bool isDragging = false;
-
-	private Ray ray;
-	private RaycastHit hit = new RaycastHit(); 
+	private Transform toDrag;
+	
+	
 
 	public void Update() {
 
 		if (isDragging) {
-			this.transform.position = new Vector3(curPos.x, curPos.y, this.transform.position.z);
+			transform.position = new Vector3(curPos.x, curPos.y, transform.position.z);
 		}
 	}
 
 	public void OnTouchBegan() {
-		isDragging = true;
-		ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-		if (Physics.Raycast(ray, out hit)){
-			startPos = new Vector3(hit.point.x, hit.point.y, this.transform.position.z);
+		print (name + " begin in OnTouchBegan");
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(lastTouch).position);
+		if (Physics.Raycast(ray, out hit) && (hit.collider.tag == "Draggable")){
+			print("hhhh");
+			
+//			toDrag = hit.transform;
+//				camDepth = hit.transform.position.z - Camera.main.transform.position.z;
+//				v3 = new Vector3(pos.x, pos.y, camDepth);
+//				v3 = Camera.main.ScreenToWorldPoint(v3);
+//				offset = toDrag.position - v3;
+//				dragging = true;
+			
+			startPos = new Vector3(hit.point.x, hit.point.y, transform.position.z);
 			curPos = startPos;
+			isDragging = true;
 		}
 	}
 	
 	public void OnTouchEnded() {
-		isDragging = false;
+		print (name + " end in OnTouchEnded");
 
+		if (isDragging)
+			isDragging = false;
+		
+		/*
+		isDragging = false;
 		if (CheckPos()) {
 			// the object was dragged to the right place
 			
@@ -41,16 +58,21 @@ public class DragTest : MonoBehaviour, I3dObjectTouchable {
 			// the object was dragged to the wrong place, we reset its position
 			ResetPosition();
 		}
+		*/
 	}
 
 	public void OnTouchMoved() {
-		Vector3 touchPos = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 10);
+		Vector3 touchPos = new Vector3(Input.GetTouch(lastTouch).position.x, Input.GetTouch(lastTouch).position.y, camDepth);
 		curPos = Camera.main.ScreenToWorldPoint(touchPos);
 	}
 
 	public void OnTouchStationary() {}
 
+
+
 	public void OnTouchEndedAnywhere() {
+	/*
+		print (name + " end in OnTouchEndedAnywhere");
 		isDragging = false;
 
 		if (CheckPos()) {
@@ -61,24 +83,27 @@ public class DragTest : MonoBehaviour, I3dObjectTouchable {
 		// the object was dragged to the wrong place, we reset its position
 			ResetPosition();
 		}
+	*/
 	}
 
 	public void OnTouchMovedAnywhere() {
-		Vector3 touchPos = new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 10);
-		curPos = Camera.main.ScreenToWorldPoint(touchPos);
+		//Vector3 touchPos = new Vector3(Input.GetTouch(lastTouch).position.x, Input.GetTouch(lastTouch).position.y, camDepth);
+		//curPos = Camera.main.ScreenToWorldPoint(touchPos);
 	}
 
 	public void OnTouchStationaryAnywhere() {}
 
 
-	public void ResetPosition() {
-		this.transform.position = startPos;
-	}
 
+	public void ResetPosition() {
+		transform.position = startPos;
+	}
+	
 	public bool CheckPos() {
-		ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+		Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(lastTouch).position);
+		RaycastHit hit;
 		if (Physics.Raycast(ray, out hit)){
-			endPos = new Vector3(hit.point.x, hit.point.y, this.transform.position.z);
+			endPos = new Vector3(hit.point.x, hit.point.y, transform.position.z);
 		}
 
 		if (endPos.x > 7)
