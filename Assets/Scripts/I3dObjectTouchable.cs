e4503e43dd92b7b50cca75b8bca9f7375ecbe68f
mcelikbas <mikail.celikbas@gmail.com>
2014-05-14 20:31:00 -0400

    added drag funtionality

---
+++ Assets/Scripts/I3dObjectTouchable.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface I3dObjectTouchable {
+
+	void OnTouchBegan();
+	void OnTouchEnded();
+	void OnTouchMoved();
+	void OnTouchStationary();
+
+
+	void OnTouchEndedAnywhere();
+	void OnTouchMovedAnywhere();
+	void OnTouchStationaryAnywhere();
+}
