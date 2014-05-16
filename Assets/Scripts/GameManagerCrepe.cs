21dfe0b450e6ac205fda436d2cbc0b69c2d82607
MaGOs92 <guillaume_fay@hotmail.com>
2014-05-16 12:42:12 -0400

    Création gameManager

---
+++ Assets/Scripts/GameManagerCrepe.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerCrepe : MonoBehaviour
+{
+
+    #region attributs
+
+    public enum GameState
+    {
+        queteNoemie,
+        preparationPate,
+        etalerLeBeurre,
+        cuissonCrepe
+    }
+
+    GameState curGameState;
+    GameState prevGameState;
+
+    GameObject[] listeIngQuete;
+
+    GameObject[] listeIngSaladier;
+
+    #endregion
+
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
