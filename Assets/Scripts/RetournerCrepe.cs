8e0f85478275bae503e5bcabd7740c0c8dcb83b5
Aiola13 <barth.13arles@gmail.com>
2014-05-23 01:30:40 -0400

    Animation Crepe Debug

---
+++ Assets/Scripts/RetournerCrepe.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RetournerCrepe : MonoBehaviour {
+
+	float counter = 0.0f;
+	//bool time = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(GameManagerCrepe.curGameState == GameManagerCrepe.GameState.cuissonCrepe && Input.acceleration.y <= -0.25 && timer){	
+			//if (timer == true){
+				counter += Time.deltaTime;
+			//}
+			
+			if (counter >= 5.0f){
+				animation.Play("RetournerCrepeAnim");
+			}
+		}
+	}
+}
