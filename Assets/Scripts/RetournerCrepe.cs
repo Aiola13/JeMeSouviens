a5eb0aa176e7a3cea42f80df83c1213ed5e9ee55
skypion <jerome.scipion1@uqac.ca>
2014-05-29 00:06:39 +0200

    Update RetournerCrepe.cs

--- Assets/Scripts/RetournerCrepe.cs
+++ Assets/Scripts/RetournerCrepe.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class RetournerCrepe : MonoBehaviour {
@@ -18,10 +18,22 @@ public class RetournerCrepe : MonoBehaviour {
 		if(GameManagerCrepe.curGameState == GameManagerCrepe.GameState.cuissonCrepe){
             counter += Time.deltaTime;
             GameManagerCrepe.cuisson.Play();
-			
-			if (Input.acceleration.y <= -0.25 && counter >= 5.0f  && !isCook){
-				animation.Play("RetournerCrepeAnim");
-				isCook = true;
+
+
+			if(counter >= 5.0f){
+				if(isCook){
+						gameObject.renderer.material.color = Color.black;
+					}
+				else{
+					gameObject.renderer.material.color = Color.green;
+
+					if (Input.acceleration.y <= -0.95 ){
+							animation.Play("RetournerCrepeAnim");
+							counter = 0.0f; 	
+						isCook = true;
+							
+						}
+				}
 			}
 		}
 	}
