7c60c183b8dc3482611239b2049c7816cb674cf7
mcelikbas <mikail.celikbas@gmail.com>
2014-06-02 15:50:20 -0400

    added GestureManager

---
+++ Assets/Scripts/GestureManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GestureManager : MonoBehaviour {
+
+	Gesture _gesture;
+	bool canDraw = false;
+
+	void Awake () {
+		_gesture = GetComponent<Gesture>();
+		_gesture.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		if (Input.GetButtonUp("Jump")) {
+			// disable script
+		    if (canDraw) {
+				_gesture.enabled = false;
+				canDraw = false;
+			}
+			// enable script
+			else {
+				_gesture.enabled = true;
+				canDraw = true;
+			}
+		}
+	}
+}
